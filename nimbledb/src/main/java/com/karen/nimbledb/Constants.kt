4801514e0f291b1aa58aef7e0883c6ccadb381bb
Karen <59339575+kargoz@users.noreply.github.com>
2020-03-10 23:00:13 +0400

    initial commit

--- nimbledb/src/main/java/com/karen/nimbledb/Constants.kt
+++ nimbledb/src/main/java/com/karen/nimbledb/Constants.kt
@@ -1,6 +1,6 @@
 package com.karen.nimbledb
 
 internal object Constants {
-    const val KS_EXTENSION = ".ks"
-    const val ROOT_DIRECTORY_NAME = "ks_cache"
+    const val KS_EXTENSION = ".nim"
+    const val ROOT_DIRECTORY_NAME = "nimble_db"
 }
